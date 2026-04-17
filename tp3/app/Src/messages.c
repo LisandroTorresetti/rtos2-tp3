e2f964d84d66af7615f8f4a30798e9f529e772ed
Lisandro Torresetti <lisandrotorresetti@gmail.com>
2026-04-16 23:48:47 -0300

    add functions to create messages

---
+++ tp3/app/Src/messages.c
@@ -0,0 +1,12 @@
+#include "messages.h"
+#include <stdio.h>
+
+static char* TICK_MSG_TEMPLATE = "{\"id\":\"%d\",\"stamp\":%d}";
+
+bool is_valid_message_type(uint8_t msg_type) {
+	return msg_type >= TICK_MSG && msg_type <= RESPONSE_MSG;
+}
+
+void build_tick_message(char* msg, const uint32_t stamp_ms) {
+	snprintf(msg, MAX_TICK_MSG_LEN, TICK_MSG_TEMPLATE, TICK_MSG, stamp_ms);
+}
