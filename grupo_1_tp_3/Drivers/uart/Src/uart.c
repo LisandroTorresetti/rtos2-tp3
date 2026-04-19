6cac591a2436efd34c2ff020a350bffc210bdb28
Ignacio Carol Lugones <ignacioclugones@gmail.com>
2026-04-19 18:00:25 -0300

    Add code for interruptions uart

--- grupo_1_tp_3/Drivers/uart/Src/uart.c
+++ grupo_1_tp_3/Drivers/uart/Src/uart.c
@@ -5,7 +5,7 @@
 
 #define UART_BUFFER_SIZE 128
 #define timeout (portMAX_DELAY)
-#define USE_INTERRUPTS 0
+#define USE_INTERRUPTS 1
 
 #if USE_INTERRUPTS
 
@@ -15,7 +15,7 @@ typedef struct {
 	volatile uint16_t tail;
 } ring_buffer_t;
 
-static UART_HandleTypeDef UartHandle;
+extern UART_HandleTypeDef huart2;
 
 static ring_buffer_t rxBuffer = {0};
 static ring_buffer_t txBuffer = {0};
@@ -29,24 +29,37 @@ static xSemaphoreHandle txSem;     // counts free space
 static xSemaphoreHandle txDoneSem; // signals TX completion/idle
 
 app_err_t uart_init() {
-	/* Create semaphores */
-	rxSem = xSemaphoreCreateCounting(UART_BUFFER_SIZE, 0);
-	if (rxSem == NULL) {
-		APP_FAIL;
-	}
-	txSem = xSemaphoreCreateCounting(UART_BUFFER_SIZE, UART_BUFFER_SIZE);
-	if (txSem == NULL) {
-		APP_FAIL;
-	}
-	txDoneSem = xSemaphoreCreateCounting(1, 1);
-	if (txDoneSem == NULL) {
-		APP_FAIL;
-	}
 
-	if (HAL_OK != HAL_UART_Receive_IT(&UartHandle, &rxByte, 1)) {
-		APP_FAIL;
-	}
-	return APP_OK;
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+
+    if (HAL_UART_Init(&huart2) != HAL_OK) {
+        return APP_FAIL;
+    }
+
+    rxSem = xSemaphoreCreateCounting(UART_BUFFER_SIZE, 0);
+    txSem = xSemaphoreCreateCounting(UART_BUFFER_SIZE, UART_BUFFER_SIZE);
+    txDoneSem = xSemaphoreCreateCounting(1, 1);
+
+    if (!rxSem || !txSem || !txDoneSem) {
+        return APP_FAIL;
+    }
+
+    /* Enable interrupt in NVIC */
+    HAL_NVIC_SetPriority(USART2_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(USART2_IRQn);
+
+    if (HAL_UART_Receive_IT(&huart2, &rxByte, 1) != HAL_OK) {
+        return APP_FAIL;
+    }
+
+    return APP_OK;
 }
 
 static void ringPush(ring_buffer_t *rb, uint8_t data) {
@@ -74,7 +87,7 @@ app_err_t uart_send(char* pstring, uint16_t size) {
 	/* Trigger transmission if idle */
 	if (xSemaphoreTake(txDoneSem, 0) == pdPASS) {
 		txByte = ringPop(&txBuffer);
-		HAL_UART_Transmit_IT(&UartHandle, &txByte, 1);
+		HAL_UART_Transmit_IT(&huart2, &txByte, 1);
 	}
 
 	return APP_OK;
@@ -103,7 +116,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 		xSemaphoreGiveFromISR(rxSem, &xHigherPriorityTaskWoken);
 		portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 
-		HAL_UART_Receive_IT(&UartHandle, &rxByte, 1);
+		HAL_UART_Receive_IT(&huart2, &rxByte, 1);
 	}
 }
 
@@ -118,7 +131,7 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
 			xSemaphoreGiveFromISR(txSem, &xHigherPriorityTaskWoken);
 			portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 
-			HAL_UART_Transmit_IT(&UartHandle, &txByte, 1);
+			HAL_UART_Transmit_IT(&huart2, &txByte, 1);
 
 		} else {
 			/* Transmission finished */
@@ -137,7 +150,7 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
 #include "string.h"
 #include <stdio.h>
 #include <stdbool.h>
-static UART_HandleTypeDef UartHandle;
+static UART_HandleTypeDef huart2;
 const uint32_t TIMEOUT = 3000;
 static void errorIfNeeded(void* delay) {
 	if (delay == NULL) {
@@ -160,24 +173,24 @@ static uint16_t findCharIndex(char *pstring, char charToFind, uint16_t max) {
 app_err_t uart_send(char* pstring, uint16_t size) {
 	errorIfNeeded(pstring);
 	uint16_t i = findCharIndex(pstring, '\0', size + 5);
-	return (HAL_OK == HAL_UART_Transmit(&UartHandle, pstring, i < size ? i : size, TIMEOUT)) ? APP_OK : APP_ERR_INTERNAL;
+	return (HAL_OK == HAL_UART_Transmit(&huart2, pstring, i < size ? i : size, TIMEOUT)) ? APP_OK : APP_ERR_INTERNAL;
 }
 
 app_err_t uart_receive(char* pstring, uint16_t size) {
 	errorIfNeeded(pstring);
-	return (HAL_OK == HAL_UART_Receive(&UartHandle, pstring, size, TIMEOUT)) ? APP_OK : APP_ERR_INTERNAL;
+	return (HAL_OK == HAL_UART_Receive(&huart2, pstring, size, TIMEOUT)) ? APP_OK : APP_ERR_INTERNAL;
 }
 
 app_err_t uart_init() {
-	UartHandle.Instance = USART2;
-	UartHandle.Init.BaudRate = 115200;
-	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
-	UartHandle.Init.StopBits = UART_STOPBITS_1;
-	UartHandle.Init.Parity = UART_PARITY_NONE;
-	UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	UartHandle.Init.Mode = UART_MODE_TX_RX;
-	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
-	bool status = HAL_UART_Init(&UartHandle) == HAL_OK;
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	bool status = HAL_UART_Init(&huart2) == HAL_OK;
 	if (status) {
 		char uart_json[256];
 
@@ -191,12 +204,12 @@ app_err_t uart_init() {
 			"\"Mode\":\"%ld\","
 			"\"Instance\":\"%s\""
 			"}\n",
-			UartHandle.Init.BaudRate,
-			UartHandle.Init.WordLength,
-			UartHandle.Init.StopBits,
-			UartHandle.Init.Parity,
-			UartHandle.Init.HwFlowCtl,
-			UartHandle.Init.Mode,
+			huart2.Init.BaudRate,
+			huart2.Init.WordLength,
+			huart2.Init.StopBits,
+			huart2.Init.Parity,
+			huart2.Init.HwFlowCtl,
+			huart2.Init.Mode,
 			"USART2"
 		);
 		uint8_t i = findCharIndex(uart_json, '\n', 256);
